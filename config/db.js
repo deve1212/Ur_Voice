27f4dc2aa1ea0bc4999f04f3c0874fc1b5473f88
devender <devender.bhatt@idfy.com>
2019-08-07 21:04:58 +0530

    initial setup for mongo lab

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {  url :"mongodb+srv://devender:XtqGjOvBxITae8Ws@emailsdata-dsjx9.mongodb.net/test?retryWrites=true&w=majority"};
+
+
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router = (database) => {router.get('/', function(req, res, next) {
+  console.log(database)
   res.render('index', { title: 'Express' });
-});
+});}
 
 module.exports = router;

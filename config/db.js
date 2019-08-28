eed1a51a6d0cad58a5d277e3538a490c558d6db5
Devender Bhatt <davebhatt69@gmail.com>
2019-08-28 18:30:27 +0530

    database intialized

--- config/db.js
+++ config/db.js
@@ -1,3 +1,16 @@
-module.exports = {  url :"mongodb+srv://devender:XtqGjOvBxITae8Ws@emailsdata-dsjx9.mongodb.net/test?retryWrites=true&w=majority"};
-
-
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb+srv://devender:XtqGjOvBxITae8Ws@emailsdata-dsjx9.mongodb.net/test?retryWrites=true&w=majority"
+module.exports = { init: ()=>{
+ return new Promise((resolve, reject) => {
+	MongoClient.connect(url, function(err, client) {
+	  if (err) 
+	    { 
+	      console.warn('-------------found errors during mongodb connection--------')
+	      return console.log(err);
+	    }
+	   dbconnection = client.db('test')
+	  	resolve(dbconnection);
+		});
+	});
+}
+}
